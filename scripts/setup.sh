f70774c5bdf3f61b311ce4a8a1767f32fcf77bbb
Kelvin Petry <test@test.de>
2020-08-07 23:03:36 +0200

    splitting packer json files for easier development

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/bash -x
 
+set -e
+
 #normal update
-sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm pacutils unzip
+sudo pacinstall --no-confirm --resolve-conflicts=all --sysupgrade
 
 #keybinding to DE
 sudo localectl set-keymap de
 
 #guest additions
-sudo /usr/bin/pacman -S --noconfirm virtualbox-guest-utils
+sudo pacman -S --noconfirm virtualbox-guest-utils
 
-#zsh
-sudo pacman -S zsh zsh-completions --noconfirm
+#zsh & powerlevel10k
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+sudo pacman -S zsh zsh-completions --noconfirm
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-sudo usermod --shell $(which zsh) $1
+sudo usermod --shell $(which zsh) $USER
 
 #terminal prog
 sudo pacman -S tilix --noconfirm
@@ -24,6 +26,15 @@ sudo pacman -S tilix --noconfirm
 sudo pacman -S firefox --noconfirm
 
 #desktop size
-xrandr --output VGA-1 --mode 1920x1080
+xrandr --output VGA-1 --mode 1360x768
 
+#make clipboard work always
+sudo echo 'VBoxClient-all' | sudo tee -a /etc/rc.local 
 
+#download sourcecode font
+# curl -L -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip
+# unzip -q SourceCodePro.zip -d ./sourceCodefont/
+# mv 'Sauce Code Pro Nerd Font Complete.ttf' /usr/share/fonts
+
+#replace i3 config
+mv i3-config.txt ../.i3/config
