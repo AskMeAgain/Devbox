0c3ab09de1b7eb0e680db69e749c4ec04f7c06d5
Kelvin Petry <test@test.de>
2020-08-14 22:52:04 +0200

    added ranger theme, rofi theme

--- scripts/ranger/colorscheme.py
+++ scripts/ranger/colorscheme.py
@@ -1,115 +1,230 @@
 from ranger.gui.colorscheme import ColorScheme
+
 from ranger.gui.color import *
+
+
 
 class ColorScheme(ColorScheme):
 
+
+
     def use(self, context):
+
         fg, bg, attr = default_colors
 
+
+
         if context.reset:
+
             return default_colors
 
+
+
         elif context.in_browser:
-            if context.selected:
-                attr = reverse
-            else:
-                attr = normal
+
+            fg = black
+
             if context.empty or context.error:
-                fg = 6
-                bg = 1
+
+                fg = black
+
             if context.border:
-                fg = white
+
+                fg = black
+
             if context.image:
-                fg = 201
+
+                fg = blue
+
             if context.video:
-                fg = 13
+
+                fg = blue
+
             if context.audio:
-                fg = 10
+
+                fg = blue
+
             if context.document:
-                fg = 12
+
+                fg = blue
+
             if context.container:
-                attr |= bold
-                fg = 1
+
+                fg = blue
+
             if context.directory:
-                attr |= normal
-                fg = 3
+
+                fg = black
+
             elif context.executable and not \
+
                     any((context.media, context.container,
+
                        context.fifo, context.socket)):
+
                 attr |= bold
-                fg = 2
+
+                fg = blue
+
             if context.socket:
-                fg = 21
+
+                fg = blue
+
                 attr |= bold
+
             if context.fifo or context.device:
-                fg = 21
+
+                fg = red
+
                 if context.device:
+
                     attr |= bold
+
             if context.link:
-                fg = context.good and 6 or 1
+
+                fg = context.good and red or red
+
             if context.bad:
+
                 fg = 0
-                bg = 1
+
+                bg = green
+
             if context.tag_marker and not context.selected:
+
                 attr |= bold
+
                 fg = 88
+
             if not context.selected and (context.cut or context.copied):
+
                 attr = reverse
+
             if context.main_column:
+
                 if context.selected:
+
                     attr |= bold
+
                 if context.marked:
+
                     attr |= bold
+
                     fg = 8
+
             if context.badinfo:
+
                 if attr & reverse:
+
                     bg = 1
+
                 else:
-                    fg = 7
+
+                    fg = red
+
+            if context.selected:
+
+                attr = bold
+
+                if fg == black:
+
+                    fg = white
+
+                bg = black
+
+            else:
+
+                attr = normal
+
+           
 
         elif context.in_titlebar:
-            attr |= bold
+
+            attr |= normal
+
             if context.hostname:
-                fg = context.bad and 1 or 5
+
+                fg = context.bad and black or black
+
             elif context.directory:
-                fg = 5
+
+                fg = black
+
             elif context.tab:
+
                 if context.good:
+
                     bg = 2
 
+
+
         elif context.in_statusbar:
+
             if context.permissions:
+
                 if context.good:
-                    fg = 2
-                    bg = 0
+
+                    fg = black
+
                 elif context.bad:
-                    fg = 1
+
+                    fg = red
+
             if context.marked:
+
                 attr |= bold | reverse
+
                 fg = 88
+
             if context.message:
+
                 if context.bad:
+
                     attr |= bold
+
                     fg = 9
+
             if context.loaded:
+
                 bg = 1
+
+
+
 
 
         if context.text:
+
             if context.highlight:
+
                 attr |= reverse
+
+
 
         if context.in_taskview:
+
             if context.title:
+
                 fg = 4
 
+
+
             if context.selected:
+
                 attr |= reverse
 
+
+
             if context.loaded:
+
                 if context.selected:
+
                     fg = 1
+
                 else:
+
                     bg = 1
 
+
+
         return fg, bg, attr
+
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ sudo -S pacman -S --noconfirm pacutils unzip
 sudo pacinstall --no-confirm --resolve-conflicts=all --sysupgrade
 
 #guest additions
-sudo pacman -S --noconfirm virtualbox-guest-utils xclip feh firefox ranger
+sudo pacman -S --noconfirm virtualbox-guest-utils xclip feh firefox ranger rofi
 sudo pacman -R --noconfirm pcmanfm
 
 #zsh & powerlevel10k
@@ -15,11 +15,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 sudo usermod --shell $(which zsh) $USER
 chsh -s $(which zsh) <<< "1234"
-mv zshrc.txt ../.zshrc
-mv p10kzsh.txt ../.p10k.zsh
+mv shells/zshrc.txt ../.zshrc
+mv shells/p10kzsh.txt ../.p10k.zsh
 
 #set themes
-mv i3-config.txt ../.i3/config
+mv i3/i3-config.txt ../.i3/config
 echo "opacity-rule = [" | sudo tee -a /etc/xdg/picom.conf
 echo "    \"90:class_g = 'tilix' && focused\"," | sudo tee -a /etc/xdg/picom.conf
 echo "    \"60:class_g = 'tilix' && !focused\"" | sudo tee -a /etc/xdg/picom.conf
@@ -28,21 +28,24 @@ echo "];" | sudo tee -a /etc/xdg/picom.conf
 #install gtk theme
 sudo tar -C /usr/share/themes -xvf Orchis-light.tar.xz
 cp gtk-2.txt gtk-2.new
-mv gtk-2.txt ../.gtkrc-2.0
-mv gtk3settings.txt ../.config/gtk-3.0/settings.ini
+mv theme/gtk-2.txt ../.gtkrc-2.0
+mv theme/gtk3settings.txt ../.config/gtk-3.0/settings.ini
 
 # so transparency works
 sed -i 's/paint/#paint/g' ../.config/compton.conf
 
 #terminal prog tilix
 sudo pacman -S tilix --noconfirm || true
-dconf load /com/gexperts/Tilix/ < ./tilix.dconf
+dconf load /com/gexperts/Tilix/ < ./tilix/tilix.dconf
 mkdir ../.config/tilix
-mv bookmarks.json ../.config/tilix/bookmarks.json
-#mv tilix-color-scheme.txt ../.config/tilix/scheme.json
+mv tilix/bookmarks.json ../.config/tilix/bookmarks.json
 
 #ranger
 mkdir ../.config/ranger/colorschemes
 mv ranger/commands.py ../.config/ranger/commands.py
 mv ranger/rcconf.txt ../.config/ranger/rc.conf
 mv ranger/colorscheme.py ../.config/ranger/colorschemes/orchid.py
+
+#rofi
+mkdir ../.config/rofi
+mv rofi/config.rasi ../.config/rofi/config.rasi
