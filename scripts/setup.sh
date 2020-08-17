f023143009263bc91bc25cc84ee575a4b8fbeea2
Kelvin Petry <test@test.de>
2020-08-17 22:11:18 +0200

    added screenshot tool, made ranger default, split settings

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,7 @@ sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syyu
 sudo pacman -R --noconfirm pcmanfm
 sudo pacman -Rd --nodeps --noconfirm picom 
 #sudo pacinstall --no-confirm --resolve-conflicts=all --sysupgrade
-sudo pacman -S --noconfirm asciidoc uthash gcc meson virtualbox-guest-utils xclip feh firefox ranger rofi docker polybar zsh zsh-completions tilix
+sudo pacman -S --noconfirm asciidoc uthash gcc meson virtualbox-guest-utils xclip feh firefox ranger rofi docker polybar zsh zsh-completions tilix flameshot
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
@@ -21,7 +21,9 @@ mkdir ../.config/ranger/colorschemes
 mkdir ../.config/rofi
 mkdir ../.config/polybar
 mkdir ../.config/picom/
+mkdir ../.config/Dharkael/
 
+sudo cp ranger/ranger.desktop /usr/share/applications/ranger.desktop
 cp theme/gtk-2.txt theme/gtk-2.new
 mv theme/gtk-2.txt ../.gtkrc-2.0
 mv theme/gtk3settings.txt ../.config/gtk-3.0/settings.ini
@@ -37,7 +39,9 @@ mv rofi/orchid.rasi ../.config/rofi/orchid.rasi
 mv polybar/config ../.config/polybar/config
 mv polybar/launch.sh ../.config/polybar/launch.sh
 mv tilix/bookmarks.json ../.config/tilix/bookmarks.json
+mv flameshot/flameshot.ini ../.config/Dharkael/flameshot.ini
 
+echo 'inode/directory=ranger.desktop' >> ../.config/mimeapps.list
 sudo tar -C /usr/share/themes -xvf theme/Orchis-light.tar.xz
 sudo usermod --shell $(which zsh) $USER
 chsh -s $(which zsh) <<< "1234"
@@ -46,5 +50,8 @@ dconf load /com/gexperts/Tilix/ < ./tilix/tilix.dconf
 sudo chmod +x ../.config/polybar/launch.sh
 
 #docker
-#sudo systemctl start docker.service
-#sudo systemctl enable docker.service
+echo '1234' | sudo -S systemctl start docker.service
+sudo systemctl enable docker.service
+sudo usermod -aG docker $USER
+
+
