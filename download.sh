10238d160d36ecc6c6f25f3eac48510c92694d6b
Kelvin Petry <test@test.de>
2020-08-23 22:07:57 +0200

    first correct draft

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+echo $1 | sudo -S pacman-mirrors --fasttrack 5 && sudo pacman -Syyu --noconfirm
+
+sudo pacman -S --noconfirm yay
+
+yay --save --answerclean All
+yay --save --answerdiff None
+echo $1 | yay -S yadm-git --sudoloop --sudoflags -S
+
+yadm clone $2
