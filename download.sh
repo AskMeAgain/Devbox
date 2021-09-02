4cb805cf7bba36643264722e55056db8ca606106
Kelvin Petry <test@test.de>
2021-09-02 22:06:45 +0200

    fixing script

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo $1 | sudo -S pacman-mirrors --fasttrack 5 && sudo pacman -Syyu --noconfirm
 
-sudo pacman -S --noconfirm yay
+echo $1 | sudo -S pacman -S yay --noconfirm
 
 if [ $2 == "none" ]; then
   echo "No git bootstrap specified"
@@ -12,7 +12,7 @@ fi
 yay --save --answerclean All
 yay --save --answerdiff None
 
-echo $1 | yay -S yadm-git --sudoloop --sudoflags -S
+echo $1 | yay -S yadm-git --noprovides --sudoloop --sudoflags -S
 
 yadm clone $2 -f --no-bootstrap
 
