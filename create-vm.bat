bfdba374439f158e5303ddb4d4922e55f6ca8946
Kelvin Petry <test@test.de>
2021-09-09 20:23:39 +0200

    switching to manjaro 21.1

--- create-vm.bat
+++ create-vm.bat
@@ -1,27 +1,44 @@
-set firstPart=.\output-i3\
-set secondPart=.\output-updated\
-set thirdPart=.\output-additions\
+@ECHO OFF
 
+set firstPart=.\i3\output-i3\
+set secondPart=.\i3\output-updated\
+set thirdPart=.\i3\output-additions\
+
+echo.
+echo --------------- Installing packer 
+if NOT EXIST packer.exe (
+    echo --------------- downloading packer.zip 
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://releases.hashicorp.com/packer/1.7.4/packer_1.7.4_windows_amd64.zip', 'packer.zip')"
+    echo --------------- unzipping file 
+    powershell -Command "Expand-Archive packer.zip -DestinationPath .
+    echo --------------- removing zip file
+    del packer.zip
+    echo --------------- packer is ready 
+) ELSE (
+    echo --------------- packer already downloaded 
+)
+
+echo --------------- finding vm name from settings file 
 for /f "tokens=2 delims=:" %%a in ('type settings.json^|find """vmname"": "') do (
   set dateid=%%a & goto :continue
 )
 :continue
 
-echo "--------------- Building base i3 ovf ---------------"
+echo --------------- Building base i3 ovf 
 if NOT EXIST %firstPart%%dateid:~2,-2%-i3.ovf (
-    @REM packer build -force -var-file="settings.json" manjaro-i3.json
+    .\packer.exe build -force -var-file="settings.json" i3\manjaro-i3.json
 )
 
-timeout /T 1
+timeout /T 2 >NUL
 
-echo "--------------- building up to date ovf ---------------"
+echo --------------- building up to date ovf 
 if NOT EXIST %secondPart%%dateid:~2,-2%-updated.ovf (
-    @REM packer build -force -var-file="settings.json" manjaro-i3-updated.json
+    .\packer.exe build -force -var-file="settings.json" i3\manjaro-i3-updated.json
 )
 
-timeout /T 1
+timeout /T 2 >NUL
 
-echo "--------------- adding additions ---------------"
+echo --------------- adding additions 
 if NOT EXIST %thirdPart%%dateid:~2,-2%-additions.ovf (
-    packer build -force -var-file="settings.json" manjaro-i3-additions.json
+    .\packer.exe build -force -var-file="settings.json" i3\manjaro-i3-additions.json
 )
