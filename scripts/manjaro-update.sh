fea8811d8ea0d092a09e28f639f0834802e3335e
Kelvin Petry <test@test.de>
2021-09-11 16:02:07 +0200

    switching to manjaro 21.1

--- scripts/manjaro-update.sh
+++ scripts/manjaro-update.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo $1 | sudo -S pacman-mirrors --fasttrack 5 && sudo pacman -Syu --noconfirm
+echo $1 | sudo -S pacman-mirrors --fasttrack 5 
+sleep 5
+echo $1 | sudo -S rm /var/lib/pacman/db.lck
+echo $1 | sudo -S pacman -Syu --noconfirm
