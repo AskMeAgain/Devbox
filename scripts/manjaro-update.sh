8fb3963cd20aeff5eeb5feaf84e5a042c4041392
Kelvin Petry <test@test.de>
2022-04-30 12:56:52 +0200

    fixing setup a bit

--- scripts/manjaro-update.sh
+++ scripts/manjaro-update.sh
@@ -3,4 +3,4 @@
 echo $1 | sudo -S pacman-mirrors --fasttrack 5 
 sleep 5
 echo $1 | sudo -S rm /var/lib/pacman/db.lck
-echo $1 | sudo -S pacman -Syu --noconfirm
+echo $1 | sudo -S pacman -Syyu --noconfirm --overwrite=/usr/bin/dunstify
