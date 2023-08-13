dcb7290cd6686f2dd52d9a39ef0a84509cc81c9a
Kelvin Petry <test@test.de>
2023-08-13 08:59:56 +0200

    first draft

--- create-vm.bat
+++ create-vm.bat
@@ -1,45 +1,44 @@
 @ECHO OFF
 
-set firstPart=.\packer_cache\outputs\manjaro-i3\os\
-
 REM set PACKER_LOG=1
 
 echo.
-echo --------------- Installing packer 
+echo --- Installing packer ---
 if NOT EXIST packer.exe (
-    echo --------------- downloading packer.zip 
+    echo --- downloading packer.zip ---
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://releases.hashicorp.com/packer/1.8.4/packer_1.8.4_windows_amd64.zip', 'packer.zip')"
-    echo --------------- unzipping file 
+    echo --- unzipping file ---
     powershell -Command "Expand-Archive packer.zip -DestinationPath .
-    echo --------------- removing zip file
+    echo --- removing zip file ---
     del packer.zip
-    echo --------------- packer is ready 
+    echo --- packer is ready ---
 ) ELSE (
-    echo --------------- packer already downloaded 
+    echo --- packer already downloaded ---
 )
 
-echo --------------- finding vm name from settings file 
-for /f "tokens=2 delims=:" %%a in ('type settings.json^|find """vmname"": "') do (
-  set dateid=%%a & goto :continue
+echo --- extracting all the json settings ---
+for /f "tokens=1,2 delims=:{}, " %%A in (settings.json) do (
+    set "%%~A=%%~B"
 )
-:continue
 
-set vmname=%dateid:~2,-2%
-
-echo --------------- vm name is %vmname%
-echo --------------- creating shared folder
+echo --- vm name is %vmname% ---
+echo --- creating shared folder ---
 if NOT EXIST ./shared-folders/%vmname%-shared\ (
     mkdir "./shared-folders/%vmname%-shared"
 )
 
-echo --------------- Building base i3 ovf 
-if NOT EXIST %firstPart%%vmname%-temp1.ovf (
-    .\packer.exe build -force -var-file="settings.json" manjaro-i3\manjaro-i3.json || @RD /S /Q %firstPart% && exit 1
+echo --- Building base ovf image ---
+if NOT EXIST .\packer_cache\outputs\%vmname%-temp1.ovf (
+    .\packer.exe build -force -var-file="settings.json" "provisioning-os/%type%/installation.json" || @RD /S /Q .\packer_cache\outputs\ && exit 1
 ) else (
-    echo ---------------- base i3 ovf already exists
+    echo --- base ovf already exists ---
 )
+
+exit 0
 
 timeout /T 2 >NUL
 
 echo --------------- applying user configs
-.\packer.exe build -force -var-file="settings.json" manjaro-i3\manjaro-i3-user-config.json
+.\packer.exe build -force -var-file="settings.json" packer/software-configuration.json
+
+pause
